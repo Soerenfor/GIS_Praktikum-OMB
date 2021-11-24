35de4b6aef16794e323abc96b2bf2ed0a2f5a665
Soerenfor <soeren.forstreuter@hs-furtwangen.de>
2021-11-24 15:17:58 +0100

    aufagbe4

--- aufgabe4/domtabelle.js
+++ aufgabe4/domtabelle.js
@@ -6,116 +6,28 @@ var EventTabelle;
     const display = document.getElementById("display");
     const button = document.getElementById("enter-Button");
     button.addEventListener("click", buttonHandler);
-    class einzelEvent {
-        interpret;
-        price;
-        constructor(interpret, price) {
-            this.interpret = interpret;
-            this.price = price;
-        }
-        Interpretzurück() {
-            return this.interpret;
-        }
-        Pricezurück() {
-            return this.price;
-        }
-        tabelleEintragen(event) {
-            let interpretValue = String(event.Interpretzurück);
-            let priceValue = Number(event.Pricezurück);
-            const neuLöschen = document.createElement("button");
-            neuLöschen.textContent = "Lösch-Event";
-            neuLöschen.style.color = "black";
-            neuLöschen.className = "lösch-Button";
-            neuLöschen.type = "submit";
-            const neueInterpretElement = document.createElement("td");
-            neueInterpretElement.textContent = interpretValue;
-            const neuePriceElement = document.createElement("td");
-            neuePriceElement.textContent = String(priceValue);
-            const neueReihe = document.createElement("tr");
-            display.appendChild(neueReihe);
-            neueReihe.appendChild(neueInterpretElement);
-            neueReihe.appendChild(neuePriceElement);
-            neueReihe.appendChild(neuLöschen);
-            Eventszusammen.storeEvent(new Event(interpretValue, priceValue));
-            function löschButtonHandler() {
-                neueReihe.removeChild(neueInterpretElement);
-                neueReihe.removeChild(neuePriceElement);
-                neueReihe.removeChild(neuLöschen);
-            }
-        }
-        show() {
-            return `Der Eintritt bei ${this.interpret} kostet ${this.price}`;
-        }
-    }
-    class Eventszusammen {
-        static events = [];
-        static loadEventszusammen() {
-            let eventsJSON = localStorage.getItem("events" || "[]");
-            for (let event of JSON.parse(eventsJSON)) {
-                this.events[this.events.length] = new Event(event.x, event.y);
-            }
-        }
-        static tabelleFüllen() {
-            for (let event of this.events) {
-                Eventszusammen.events.tabelleEintragen(event);
-            }
-        }
-        static storeEvent(event) {
-            this.events.push(event);
-            this.events[this.events.length] = event;
-            localStorage.setItem("events", JSON.stringify(this.events));
-        }
-    }
-    Eventszusammen.loadEvents();
-    Eventszusammen.tabelleFüllen();
     function buttonHandler() {
         let interpretValue = inputIntpret.value;
         let priceValue = Number(inputPrice.value);
         const neuLöschen = document.createElement("button");
-        neuLöschen.textContent = "Lösch-Event";
+        neuLöschen.textContent = "Event-Löschen";
         neuLöschen.style.color = "black";
-        neuLöschen.className = "lösch-Button";
+        neuLöschen.className = "löschButton";
         neuLöschen.type = "submit";
         neuLöschen.addEventListener("click", löschButtonHandler);
         const neueInterpretElement = document.createElement("td");
         neueInterpretElement.textContent = interpretValue;
         const neuePriceElement = document.createElement("td");
         neuePriceElement.textContent = String(priceValue);
-        const neueReihe = document.createElement("tr");
-        display.appendChild(neueReihe);
-        neueReihe.appendChild(neueInterpretElement);
-        neueReihe.appendChild(neuePriceElement);
-        neueReihe.appendChild(neuLöschen);
-        Eventszusammen.storeEvent(new Event(interpretValue, priceValue));
+        const neueZeile = document.createElement("tr");
+        display.appendChild(neueZeile);
+        neueZeile.appendChild(neueInterpretElement);
+        neueZeile.appendChild(neuePriceElement);
+        neueZeile.appendChild(neuLöschen);
         function löschButtonHandler() {
-            neueReihe.removeChild(neueInterpretElement);
-            neueReihe.removeChild(neuePriceElement);
-            neueReihe.removeChild(neuLöschen);
-        }
-    }
-    function tabelleFüllen(event) {
-        let interpretValue = String(event.Interpretzurück);
-        let priceValue = Number(event.Pricezurück);
-        const neuLöschen = document.createElement("button");
-        neuLöschen.textContent = "Lösch-Event";
-        neuLöschen.style.color = "black";
-        neuLöschen.className = "lösch-Button";
-        neuLöschen.type = "submit";
-        neuLöschen.addEventListener("click", löschButtonHandler);
-        const neueInterpretElement = document.createElement("td");
-        neueInterpretElement.textContent = interpretValue;
-        const neuePriceElement = document.createElement("td");
-        neuePriceElement.textContent = String(priceValue);
-        const neueReihe = document.createElement("tr");
-        display.appendChild(neueReihe);
-        neueReihe.appendChild(neueInterpretElement);
-        neueReihe.appendChild(neuePriceElement);
-        neueReihe.appendChild(neuLöschen);
-        Eventszusammen.storeEvent(new Event(interpretValue, priceValue));
-        function löschButtonHandler() {
-            neueReihe.removeChild(neueInterpretElement);
-            neueReihe.removeChild(neuePriceElement);
-            neueReihe.removeChild(neuLöschen);
+            neueZeile.removeChild(neueInterpretElement);
+            neueZeile.removeChild(neuePriceElement);
+            neueZeile.removeChild(neuLöschen);
         }
     }
 })(EventTabelle || (EventTabelle = {}));
